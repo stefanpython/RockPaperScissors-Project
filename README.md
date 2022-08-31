52f3692fe71c69ee0e632b4510a3a4ce4d0236a4
stefanpython <stefanhtml@yahoo.com>
2022-08-31 19:34:54 +0300

    Edit README.md, add images, styles.css, buttons interaction

--- README.md
+++ README.md
@@ -10,3 +10,21 @@
 
 
 ###### Will return in future TOP lessons to add some visuals to the game.
+
+
+
+#### Update:
+- After finishing the DOM Manipulation and Events lecture, I`ve added some more knowledge to my brain.
+    * Added an UI to the game.
+    * I learned how to:
+        * add event listeners for multiple buttons.
+        * use query selectors.
+        * how to change a div text with textContent/innerHTML.
+        * use .remove() function to remove buttons after finishing a process
+        * use location.reload() to refresh the page
+        * add 'visibility' or 'hidden' with .style.visibilty = ''.
+        * and a few more things.
+
+###### The project was a bit hard for me at the beginning, but a few hours in and I was rolling. This was a fun project and I loved every minute of it.
+
+### Special Thanks to the Community for making this possible for me and others.
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
 
     let items = [
-        'Rock',
-        'Paper',
-        'Scissors'
+        'ROCK',
+        'PAPER',
+        'SCISSORS'
     ];
 
     let randomItem = items[Math.floor(Math.random() * items.length)];
@@ -11,67 +11,94 @@ function getComputerChoice() {
     return randomItem;
 }
 
+
 function playRound(playerSelection, computerSelection) {
     let roundWinner='';
 
-    if (playerSelection == 'rock'.toUpperCase() && computerSelection == 'Rock') {
-        roundWinner = "It`s a tie"
-    }else if (playerSelection == 'rock'.toUpperCase() && computerSelection == 'Paper') {
-         rroundWinner = "You lose! Paper beats Rock"
-    } else if (playerSelection == 'rock'.toUpperCase() && computerSelection == 'Scissors'){
-        roundWinner = "You win! Rock beats Paper"
-    } else if (playerSelection == 'paper'.toUpperCase() && computerSelection == 'Rock') {
-        roundWinner = "You win! Paper beats Rock"
-    } else if (playerSelection == 'paper'.toUpperCase() && computerSelection == 'Paper') {
-        roundWinner = "It`s a tie"
-    } else if (playerSelection == 'paper'.toUpperCase() && computerSelection == 'Scissors') {
-        roundWinner = "You lose! Scissors beats Paper"
-    } else if (playerSelection == 'scissors'.toUpperCase() && computerSelection == 'Rock') {
-        roundWinner = "You lose! Rock beats Scissors"
-    } else if (playerSelection == 'scissors'.toUpperCase() && computerSelection == 'Paper') {
-        roundWinner = "You win! Scissors beats Paper"
-    } else if (playerSelection == 'scissors'.toUpperCase() && computerSelection == 'Scissors') {
-        roundWinner = "It`s a tie"
+    if (playerSelection == 'rock'.toUpperCase() && computerSelection == 'ROCK') {
+        return "It`s a tie"
+    }else if (playerSelection == 'rock'.toUpperCase() && computerSelection == 'PAPER') {
+        return "You lose! Paper beats Rock"
+    } else if (playerSelection == 'rock'.toUpperCase() && computerSelection == 'SCISSORS'){
+        return "You win! Rock beats Paper"
+    } else if (playerSelection == 'paper'.toUpperCase() && computerSelection == 'ROCK') {
+        return "You win! Paper beats Rock"
+    } else if (playerSelection == 'paper'.toUpperCase() && computerSelection == 'PAPER') {
+        return "It`s a tie"
+    } else if (playerSelection == 'paper'.toUpperCase() && computerSelection == 'SCISSORS') {
+        return "You lose! Scissors beats Paper"
+    } else if (playerSelection == 'scissors'.toUpperCase() && computerSelection == 'ROCK') {
+        return "You lose! Rock beats Scissors"
+    } else if (playerSelection == 'scissors'.toUpperCase() && computerSelection == 'PAPER') {
+        return "You win! Scissors beats Paper"
+    } else if (playerSelection == 'scissors'.toUpperCase() && computerSelection == 'SCISSORS') {
+        return "It`s a tie"
      } 
-     return roundWinner;
+    return roundWinner;
+    
 }
 
+const button = document.querySelectorAll('button');
+const score = document.querySelector(".score");
+const winner = document.querySelector(".winner");
+score.textContent = "Chose your weapon below:";
 
+// Div with all buttons
+const btns = document.querySelector(".btn");
 
-// playerSelection = prompt("Rock, Paper or Scrissors?").toUpperCase();
-
+// Try again button image
+const replay = document.querySelector('.img');
+replay.style.visibility = 'hidden';
 
 function game() {
     let player = 0;
     let computer = 0;
     let roundResult = '';
-   
-
-    for (let i = 0; i < 5; i++) {
-        
-        playerSelection = prompt("Rock, Paper or Scrissors?").toUpperCase();
-        computerSelection = getComputerChoice();
-        roundResult = playRound(playerSelection, computerSelection);
-
-        if (roundResult.slice(0, 8) === 'You win!') {
-            player += 1;
-            console.log("You won this round!" + " -- Score: " + "Player: " + player + " - " + "Computer: " + computer)
-        } else if (roundResult.slice(0, 8) === 'You lose') {
-            computer += 1;
-            console.log("You lost this round!" + " -- Score: " + player + " - " + computer)
-        } else {
-            console.log("It`s a tie!")
-            i -= 1;
-        }
-        
-    }
+    let i = 0;
     
-    if (player > computer) {
-        console.log("You won the game!")
-    } else {
-        console.log("You lost the game!")
-    } 
+    
+    
+    button.forEach((button) => {
+              button.addEventListener('click', () => {
+              let playerSelection = button.id
+              computerSelection = getComputerChoice();
+              roundResult = playRound(playerSelection, computerSelection);
+              console.log(roundResult);
 
-}
+            
+                
+              if (roundResult.slice(0, 8) === 'You win!') {
+                player += 1;
+                score.textContent = "Score: " + "Player: " + player + "  -  " + "Computer: " + computer
+            } else if (roundResult.slice(0, 8) === 'You lose') {
+                computer += 1;
+                score.textContent = "Score: " + "Player: " + player + "  -  "  + "Computer: " + computer
+            } else {
+                player = player;
+                computer = computer;
+                score.textContent = "It`s a tie!";
+                
+            }
+
+            
+
+            if (player === 5) {
+                score.textContent = 'You are the WINNER!';
+                btns.remove();
+                replay.style.visibility = 'visible';
+            } else if (computer === 5) {
+                score.textContent = 'You Lost the game!';
+                btns.remove()
+                replay.style.visibility = 'visible';
+            }
+              })
+              
+        })
+
+        replay.addEventListener('click', () => {
+            location.reload();
+        })
+       
+    }
 
 game();
